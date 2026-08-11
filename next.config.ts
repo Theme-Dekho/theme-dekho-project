085a3faf4e186cc3815a7092c31d6191d0a4289b
manishkt52 <manishkt52@gmail.com>
2026-08-11 13:55:23 +0530

    modified: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination:
+          "https://theme-dekho-backend.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
